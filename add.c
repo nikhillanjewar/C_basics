f26e598849ab002a62a520d15487625d6779cc8d
Nikhil Lanjewar <105858734+nikhillanjewar@users.noreply.github.com>
2023-04-06 16:13:56 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int a, b;
+	printf("Enter the Value of a: ");
+	scanf("%d",&a);
+	
+	printf("Enter the Value of b: ");
+	scanf("%d",&b);
+	
+	printf("The Addition of two number is %d\n", a+b);
+	
+	return 0;
+	
+}
---
+++ arithmetic_operators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	//initilizing of a number
+	int a,b;
+	
+	//declaration of number
+	a = 52;
+	b = 3;
+	
+	//arithmetic operators basic
+	printf("a+b: %d\n", a+b);
+	printf("a-b: %d\n", a-b);		
+	printf("a*b: %d\n", a*b);
+	printf("a/b: %d\n", a/b);
+	printf("a%b: %d\n", a%b);
+	return 0;
+}
