1aa1d56e6501268476bb9c0c0417b7cde8d1e0ab
Nikhil Lanjewar <105858734+nikhillanjewar@users.noreply.github.com>
2023-04-09 23:18:23 +0530

    Add files via upload

---
+++ Explicit_typecasting.c
@@ -0,0 +1,8 @@
+
+#include<stdio.h>
+int main(){
+	int i=0;
+	float j= (float) 9/5;
+	
+	printf("The Floating point Number is %f",j);
+}
---
+++ Implicit_typecasting.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int i=45;
+	float j= 10/2;
+	
+	printf("The value of Interger is %d", i);
+	return 0;
+}
