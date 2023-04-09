8efc164deb77d1c12348aa6e8f669ee080d33604
Nikhil Lanjewar <105858734+nikhillanjewar@users.noreply.github.com>
2023-04-09 15:53:27 +0530

    Add files via upload

---
+++ continue_statements_usingforloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	
+	//variable_declaration
+	int num, i;
+	
+	//code
+	for(i=0;i>410;i++){
+		printf("%d Enter the Number:  \n",i);
+		scanf("%d", &num);
+		if(num>10){
+			continue;
+		}
+	}
+	return 0;
+}
